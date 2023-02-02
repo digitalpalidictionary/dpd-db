345c873d088751754cef48428503b58c76bd1083
bdhrs <bodhirasa@gmail.com>
2023-02-02 17:46:30 +0530

    remove if none

--- db/dpd_db_from_csv.py
+++ db/dpd_db_from_csv.py
@@ -2,8 +2,8 @@
 
 import sys
 import csv
-import tqdm
 
+from rich import print
 from typing import Dict, List
 from pathlib import Path
 
@@ -33,32 +33,32 @@ def _csv_row_to_root(x: Dict[str, str]) -> PaliRoot:
     # x = {k:_to_empty(v) for (k,v) in x.items()}
 
     return PaliRoot(
-        root =                         x['Root']                          if x['Root'] != "" else None,
-        root_in_comps =         x['In Comps']                      if x['In Comps'] != "" else None,
-        root_has_verb =         x['V']                             if x['V'] != "" else None,
-        root_group =        int(x['Group'])                        if x['Group'] != "" else None,
-        root_sign =             x['Sign']                          if x['Sign'] != "" else None,
-        root_meaning =          x['Meaning']                       if x['Meaning'] != "" else None,
-        sanskrit_root =         x['Sk Root']                       if x['Sk Root'] != "" else None,
-        sanskrit_root_meaning = x['Sk Root Mn']                    if x['Sk Root Mn'] != "" else None,
-        sanskrit_root_class =   x['Cl']                            if x['Cl'] != "" else None,
-        root_example =          x['Example']                       if x['Example'] != "" else None,
-        dhatupatha_num =        x['Dhātupātha']                    if x['Dhātupātha'] != "" else None,
-        dhatupatha_root =       x['DpRoot']                        if x['DpRoot'] != "" else None,
-        dhatupatha_pali =       x['DpPāli']                        if x['DpPāli'] != "" else None,
-        dhatupatha_english =    x['DpEnglish']                     if x['DpEnglish'] != "" else None,
-        dhatumanjusa_num =  (x['Kaccāyana Dhātu Mañjūsā'])      if x['Kaccāyana Dhātu Mañjūsā'] != "" else None,
-        dhatumanjusa_root =     x['DmRoot']                        if x['DmRoot'] != "" else None,
-        dhatumanjusa_pali =     x['DmPāli']                        if x['DmPāli'] != "" else None,
-        dhatumanjusa_english =  x['DmEnglish']                     if x['DmEnglish'] != "" else None,
-        dhatumala_root =        x['Saddanītippakaraṇaṃ Dhātumālā'] if x['Saddanītippakaraṇaṃ Dhātumālā'] != "" else None,
-        dhatumala_pali =        x['SnPāli']                        if x['SnPāli'] != "" else None,
-        dhatumala_english =     x['SnEnglish']                     if x['SnEnglish'] != "" else None,
-        panini_root =           x['Pāṇinīya Dhātupāṭha']           if x['Pāṇinīya Dhātupāṭha'] != "" else None,
-        panini_sanskrit =       x['PdSanskrit']                    if x['PdSanskrit'] != "" else None,
-        panini_english =        x['PdEnglish']                     if x['PdEnglish'] != "" else None,
-        note =                  x['Note']                          if x['Note'] != "" else None,
-        matrix_test =           x['matrix test']                   if x['matrix test'] != "" else None,
+        root = x['Root'],
+        root_in_comps = x['In Comps'],
+        root_has_verb = x['V'],
+        root_group = int(x['Group']), 
+        root_sign = x['Sign'],
+        root_meaning = x['Meaning'],
+        sanskrit_root = x['Sk Root'],
+        sanskrit_root_meaning = x['Sk Root Mn'],
+        sanskrit_root_class = x['Cl'],
+        root_example = x['Example'],
+        dhatupatha_num =  x['Dhātupātha'],
+        dhatupatha_root = x['DpRoot'],
+        dhatupatha_pali = x['DpPāli'],
+        dhatupatha_english = x['DpEnglish'],
+        dhatumanjusa_num =  (x['Kaccāyana Dhātu Mañjūsā'])     ,
+        dhatumanjusa_root =  x['DmRoot'],
+        dhatumanjusa_pali =  x['DmPāli'],
+        dhatumanjusa_english =  x['DmEnglish'],
+        dhatumala_root =  x['Saddanītippakaraṇaṃ Dhātumālā'],
+        dhatumala_pali =  x['SnPāli'],
+        dhatumala_english =  x['SnEnglish'],
+        panini_root =  x['Pāṇinīya Dhātupāṭha'],
+        panini_sanskrit = x['PdSanskrit'],
+        panini_english =  x['PdEnglish'],
+        note =      x['Note'],
+        matrix_test =  x['matrix test'],
     )
 
 def _csv_row_to_pali_word(x: Dict[str, str]) -> PaliWord:
@@ -73,55 +73,55 @@ def _csv_row_to_pali_word(x: Dict[str, str]) -> PaliWord:
     # 'Root Meaning'
 
     return PaliWord(
-        user_id =                    x['ID']                                       if x['ID'] != "" else None,
-        pali_1 =                x['Pāli1']                                 if x['Pāli1'] != "" else None,
-        pali_2 =                x['Pāli2']                                 if x['Pāli2'] != "" else None,
-        pos =                   x['POS']                                   if x['POS'] != "" else None,
-        grammar =               x['Grammar']                               if x['Grammar'] != "" else None,
-        derived_from =          x['Derived from']                          if x['Derived from'] != "" else None,
-        neg =                   x['Neg']                                   if x['Neg'] != "" else None,
-        verb =                  x['Verb']                                  if x['Verb'] != "" else None,
-        trans =                 x['Trans']                                 if x['Trans'] != "" else None,
-        plus_case =             x['Case']                                  if x['Case'] != "" else None,
-        meaning_1 =             x['Meaning IN CONTEXT']                    if x['Meaning IN CONTEXT'] != "" else None,
-        meaning_lit =           x['Literal Meaning']                       if x['Literal Meaning'] != "" else None,
-        non_ia =                x['Non IA']                                if x['Non IA'] != "" else None,
-        sanskrit =              x['Sanskrit']                              if x['Sanskrit'] != "" else None,
-        root_key =              x['Pāli Root']                             if x['Pāli Root'] != "" else None,
-        root_sign =             x['Sgn']                                   if x['Sgn'] != "" else None,
-        root_base =                  x['Base']                                  if x['Base'] != "" else None,
-        family_root =           x['Family']                                if x['Family'] != "" else None,
-        family_word =           x['Word Family']                           if x['Word Family'] != "" else None,
-        family_compound =       x['Family2']                               if x['Family2'] != "" else None,
-        construction =          x['Construction']                          if x['Construction'] != "" else None,
-        derivative =            x['Derivative']                            if x['Derivative'] != "" else None,
-        suffix =                x['Suffix']                                if x['Suffix'] != "" else None,
-        phonetic =              x['Phonetic Changes']                      if x['Phonetic Changes'] != "" else None,
-        compound_type =         x['Compound']                              if x['Compound'] != "" else None,
-        compound_construction = x['Compound Construction']                 if x['Compound Construction'] != "" else None,
-        non_root_in_comps =     x['Non-Root In Comps']                     if x['Non-Root In Comps'] != "" else None,
-        source_1 =              x['Source1']                               if x['Source1'] != "" else None,
-        sutta_1 =               x['Sutta1']                                if x['Sutta1'] != "" else None,
-        example_1 =             x['Example1']                              if x['Example1'] != "" else None,
-        source_2 =              x['Source 2']                              if x['Source 2'] != "" else None,
-        sutta_2 =               x['Sutta2']                                if x['Sutta2'] != "" else None,
-        example_2 =             x['Example 2']                             if x['Example 2'] != "" else None,
-        antonym =               x['Antonyms']                              if x['Antonyms'] != "" else None,
-        synonym =               x['Synonyms – different word']             if x['Synonyms – different word'] != "" else None,
-        variant =               x['Variant – same constr or diff reading'] if x['Variant – same constr or diff reading'] != "" else None,
-        commentary =            x['Commentary']                            if x['Commentary'] != "" else None,
-        notes =                 x['Notes']                                 if x['Notes'] != "" else None,
-        cognate =               x['Cognate']                               if x['Cognate'] != "" else None,
-        category =              x['Category']                              if x['Category'] != "" else None,
-        link =                  x['Link']                                  if x['Link'] != "" else None,
-        stem =                  x['Stem']                                  if x['Stem'] != "" else None,
-        pattern =               x['Pattern']                               if x['Pattern'] != "" else None,
-        meaning_2 =             x['Buddhadatta']                           if x['Buddhadatta'] != "" else None,
+        user_id =        x['ID'],
+        pali_1 =    x['Pāli1'],
+        pali_2 =    x['Pāli2'],
+        pos =       x['POS'],
+        grammar =   x['Grammar'],
+        derived_from = x['Derived from'],
+        neg =       x['Neg'],
+        verb =      x['Verb'],
+        trans =     x['Trans'],
+        plus_case = x['Case'],
+        meaning_1 = x['Meaning IN CONTEXT'],
+        meaning_lit =  x['Literal Meaning'],
+        non_ia =    x['Non IA'],
+        sanskrit =  x['Sanskrit'],
+        root_key =  x['Pāli Root'],
+        root_sign = x['Sgn'],
+        root_base =      x['Base'],
+        family_root =  x['Family'],
+        family_word =  x['Word Family'],
+        family_compound = x['Family2'],
+        construction = x['Construction'],
+        derivative =   x['Derivative'],
+        suffix =    x['Suffix'],
+        phonetic =  x['Phonetic Changes'],
+        compound_type =   x['Compound'],
+        compound_construction = x['Compound Construction'],
+        non_root_in_comps =  x['Non-Root In Comps'],
+        source_1 =  x['Source1'],
+        sutta_1 =   x['Sutta1'],
+        example_1 = x['Example1'],
+        source_2 =  x['Source 2'],
+        sutta_2 =   x['Sutta2'],
+        example_2 = x['Example 2'],
+        antonym =   x['Antonyms'],
+        synonym =   x['Synonyms – different word'],
+        variant =   x['Variant – same constr or diff reading'],
+        commentary =   x['Commentary'],
+        notes =     x['Notes'],
+        cognate =   x['Cognate'],
+        category =  x['Category'],
+        link =      x['Link'],
+        stem =      x['Stem'],
+        pattern =   x['Pattern'],
+        meaning_2 = x['Buddhadatta'],
     )
 
 
 def add_pali_roots(db_session: Session, csv_path: Path):
-    print("=== add_pali_roots() ===")
+    print("[blue]add_pali_roots")
 
     rows = []
     with open(csv_path, 'r') as f:
@@ -135,15 +135,14 @@ def add_pali_roots(db_session: Session, csv_path: Path):
 
     items: List[PaliRoot] = list(map(_csv_row_to_root, filter(_is_count_not_zero_or_empty, rows)))
 
-    print("checking for dupes")
+    print("[blue]checking for dupes and adding")
     try:
-        for i in tqdm(items):
-            # print(f"PaliRoot: {i.root}")
+        for i in items:
 
             # Check if item is a duplicate.
             res = db_session.query(PaliRoot).filter_by(root = i.root).first()
             if res:
-                print(f"WARN: Duplicate found, skipping!\nAlready in DB:\n{res}\nConflicts with:\n{i}")
+                print(f"[red]Duplicate found, skipping!\nAlready in DB:\n{res}\nConflicts with:\n{i}")
                 continue
 
             db_session.add(i)
@@ -151,38 +150,37 @@ def add_pali_roots(db_session: Session, csv_path: Path):
         db_session.commit()
 
     except Exception as e:
-        print(f"ERROR: Adding to db failed:\n{e}")
+        print(f"[red]ERROR: Adding to db failed:\n{e}")
 
 def add_pali_words(db_session: Session, csv_path: Path):
-    print("=== add_pali_words() ===")
+    print("[blue]add_pali_words")
 
     rows = []
     with open(csv_path, 'r') as f:
         reader = csv.DictReader(f, delimiter='\t')
         for row in reader:
             rows.append(row)
-
+    
     items: List[PaliWord] = list(map(_csv_row_to_pali_word, rows))
 
-    print("checking for dupes")
+    print("[blue]checking for dupes and adding")
     try:
-        for i in tqdm(items):
-            # print(f"PaliWord: {i.pali1}")
-
+        for i in items:
             # Check if item is a duplicate.
-            res = db_session.query(PaliWord).filter_by(pali_1 = i.pali_1).first()
-            if res:
-                print(f"WARN: Duplicate found, skipping!\nAlready in DB:\n{res}\nConflicts with:\n{i}")
-                continue
+            # res = db_session.query(PaliWord).filter_by(pali_1 = i.pali_1).first()
+            # if res:
+            #     print(f"[red]Duplicate found, skipping!\nAlready in DB:\n{res}\nConflicts with:\n{i}")
+            #     continue
 
             db_session.add(i)
 
         db_session.commit()
 
     except Exception as e:
-        print(f"ERROR: Adding to db failed:\n{e}")
+        print(f"[red]ERROR: Adding to db failed:\n{e}")
 
 def main():
+    print("[yellow]convert dpd.csv to dpd.db")
     dpd_db_path = Path("dpd.db")
     roots_csv_path = Path("../csvs/roots.csv")
     dpd_full_path = Path("../csvs/dpd-full.csv")
@@ -194,7 +192,7 @@ def main():
 
     for p in [roots_csv_path, dpd_full_path]:
         if not p.exists():
-            print(f"File does not exist: {p}")
+            print(f"[red]File does not exist: {p}")
             sys.exit(1)
 
     db_session = get_db_session(dpd_db_path)
