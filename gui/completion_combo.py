c5399f1f6cc199bfda285587d1693d23991da1bd
Anton Karmanov <a.karmanov@inventati.org>
2023-07-15 14:41:34 +0300

    Update CompletionCombo tooltip workflow

--- gui/completion_combo.py
+++ gui/completion_combo.py
@@ -1,3 +1,8 @@
+""" Usage example:
+
+TODO
+"""
+
 import logging
 
 from typing import List, Tuple
@@ -14,6 +19,8 @@ def count_combo_size(values: List[str]) -> Tuple[int, int]:
 
 class CompletionCombo(PySimpleGUI.Combo):
     """ Combo box with simplified selection
+
+    TooltipObject member should not be changed after initialization
     """
     def __init__(self, values: List[str], *args, **kwargs) -> None:
         self.__values = values
@@ -21,28 +28,22 @@ class CompletionCombo(PySimpleGUI.Combo):
         if not size:
             self.__size = count_combo_size(values)
         kwargs['size'] = self.__size
+        initial_tooltip = kwargs.get('tooltip')
+        if not initial_tooltip:
+            self.__initial_tooltip = '\n'.join(values)
+            kwargs['tooltip'] = self.__initial_tooltip
         super().__init__(values, *args, **kwargs)
 
-    def _hide_tooltip(self) -> None:
-        self.set_tooltip(None)
+    def hide_tooltip(self) -> None:
+        self.set_tooltip(self.__initial_tooltip)
+        self.TooltipObject.hidetip()
 
-    # TODO
-    def __bind(self) -> None:
-        ...
-
-    # TODO
-    @classmethod
-    def subscribe(cls, window, event: str) -> None:
-        ...
-
-    # TODO Bind documentation
     def reset(self) -> None:
         """ Reset variants to initial state
         """
         self.update(values=self.__values, size=self.__size)
 
     # TODO Save filtered list for next filtration, drop on deleting chars
-    # TODO Bind documentation
     def filter(self) -> None:
         """ Filter values with current value
         """
@@ -58,18 +59,17 @@ class CompletionCombo(PySimpleGUI.Combo):
                 values=new_field_values,
                 size=self.__size)
             if new_field_values:
-                self.set_tooltip("\n".join(new_field_values))
+                self.set_tooltip('\n'.join(new_field_values))
                 _combo_width, combo_height = self.get_size()
                 tooltip = self.TooltipObject
                 tooltip.y += 1.5 * combo_height
                 tooltip.showtip()
             else:
-                self._hide_tooltip()
+                self.hide_tooltip()
         else:
             self.reset()
-            self._hide_tooltip()
+            self.hide_tooltip()
 
-    # TODO Bind documentation
     def drop_down(self) -> None:
         """ Drop list of values down like as the arrow button has been clicked
 
@@ -80,9 +80,8 @@ class CompletionCombo(PySimpleGUI.Combo):
         if func:
             func("<Down>")
 
-    # TODO Bind documentation
     def complete(self) -> None:
         """ Complete word with first entry of the list
         """
         self.update(set_to_index=0)
-        self._hide_tooltip()
+        self.hide_tooltip()
