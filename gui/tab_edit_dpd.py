060723cefd869275c4f632c04954b68b5559346d
bdhrs <bodhirasa@gmail.com>
2023-07-25 17:53:37 +0530

    delete button red

--- gui/tab_edit_dpd.py
+++ gui/tab_edit_dpd.py
@@ -621,7 +621,7 @@ kar + *āpe  > kārāpe > karāpe (caus, irreg).")),
             sg.Button(
                 "Delete", key="delete_button",
                 tooltip="Delete a word from the db. Careful!",
-                text_color="red"),
+                mouseover_colors="red"),
             sg.Button(
                 "Update Sandhi", key="update_sandhi_button",
                 tooltip="Update list of words with sandhi apostophes"),
