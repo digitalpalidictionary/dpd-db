75c9c8f116c6fb3a15a1000f6adfbde0bf480357
bdhrs <bodhirasa@gmail.com>
2023-07-26 17:06:55 +0530

    udpate db from some csv

---
+++ scripts/update_db_from_some_csv.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+from rich import print
+
+from db.get_db_session import get_db_session
+from db.models import PaliWord
+from tools.tsv_read_write import read_tsv_dot_dict
+
+
+db_session = get_db_session("dpd.db")
+
+# put in the path of the csv you want to open
+csv_path = Path("../csvs/dpd-full.csv")
+
+# make a dot_dict of the csv
+csv = read_tsv_dot_dict(csv_path)
+print("[yellow]first entry in the csv")
+print(csv[0])
+input("press enter to continue: ")
+
+# make a list of columns
+columns = [key for key in csv[0].keys()]
+print("[yellow]columns in the csv")
+print(columns)
+input("press enter to continue: ")
+
+
+# iterate through the csv item by item
+for i in csv:
+
+    # find the column name you want to change, in this case "Category"
+    if i.Category:
+        # this find the dpd word which matches the user_id
+        # here you could also use Russian or SBS instead of PaliWord
+        csv_user_id = i.ID
+        db_entry = db_session.query(PaliWord).filter(
+            PaliWord.user_id == csv_user_id).first()
+
+        # this updates the db entry with the csv value
+        print(f"old: {db_entry.family_set}")
+        db_entry.family_set = i.Category
+        print(f"new: {db_entry.family_set}")
+        print()
+
+# check that the output is as expected, then uncomment commit
+# db_session.commit()
+
+# dont forget to always close the db session
+db_session.close()
+
