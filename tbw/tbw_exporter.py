9512d4b5833dff6313104fdac25b24bd5f518b93
bdhrs <bodhirasa@gmail.com>
2023-08-01 05:35:57 +0530

    tbw output & readme

--- tbw/tbw_exporter.py
+++ tbw/tbw_exporter.py
@@ -24,10 +24,8 @@ def main():
     tic()
     print("[bright_yellow]dpd lookup system")
 
-    # ---------------------------------------------------------------------
     # make a set of words in sutta central texts
-    # ---------------------------------------------------------------------
-
+    
     print(f"[green]{'making sutta central ebts word list':<40}", end="")
     sc_text_list = [
         "vin1", "vin2", "vin3", "vin4", "vin5",
@@ -43,10 +41,8 @@ def main():
     text_set: set = make_sc_text_set(sc_text_list)
     print(f"{len(text_set):,}")
 
-    # ---------------------------------------------------------------------
     # get all the required info from the database
-    # ---------------------------------------------------------------------
-
+    
     print(f"[green]{'making db searches':<40}", end="")
     db_session = get_db_session(PTH.dpd_db_path)
     dpd_db = db_session.query(PaliWord)
@@ -54,10 +50,8 @@ def main():
     deconstr_db = db_session.query(Sandhi)
     print("OK")
 
-    # ---------------------------------------------------------------------
     # make a set of all words in deconstructed compounds
-    # ---------------------------------------------------------------------
-
+    
     print(f"[green]{'making deconstructor splits set':<40}", end="")
     deconstr_splits_set: set = set()
     matched = set()
@@ -69,10 +63,8 @@ def main():
                 deconstr_splits_set.update(split.split(" + "))
     print(f"{len(deconstr_splits_set):,}")
 
-    # ---------------------------------------------------------------------
     # make an inflections to headwords dictionary
-    # ---------------------------------------------------------------------
-
+    
     print(f"[green]{'making inflections to headwords dict':<40}", end="")
     i2h: dict = {}
     for counter, (i, dd) in enumerate(zip(dpd_db, dd_db)):
@@ -91,65 +83,52 @@ def main():
                     i2h[inflection] += [i.pali_1]
     print(f"{len(i2h):,}")
 
-    # ---------------------------------------------------------------------
     # save inflections to headwords json
-    # ---------------------------------------------------------------------
-
+    
     print(f"[green]{'writing inflections to headwords json':<40}", end="")
     with open(PTH.i2h_json_path, "w") as f:
         json.dump(i2h, f, ensure_ascii=False, indent=0)
         print("OK")
 
-    # ---------------------------------------------------------------------
     # make a set of unmatched words
-    # ---------------------------------------------------------------------
-
+    
     print(f"[green]{'making set of unmatched words':<40}", end="")
     unmatched = text_set - matched
     print(f"{len(unmatched):,}")
 
-    # ---------------------------------------------------------------------
     # make a set of headwords in ebts
-    # ---------------------------------------------------------------------
-
+    
     print(f"[green]{'making headwords set':<40}", end="")
     headwords_set: set = set()
     for key, values in i2h.items():
         headwords_set.update(values)
     print(f"{len(headwords_set):,}")
 
-    # ---------------------------------------------------------------------
     # make a dict of dpd data - only words in ebts
-    # ---------------------------------------------------------------------
-
+    
     print(f"[green]{'making dpd ebts dict':<40}", end="")
     dpd_dict = {}
     for i in dpd_db:
         if i.pali_1 in headwords_set:
             string = f"{i.pos}. "
             string += make_meaning_html(i)
-            if i.meaning_1:
-                if i.construction:
-                    string += f" [{summarize_constr(i)}]"
+            if i.construction:
+                string += f" [{summarize_constr(i)}]"
             dpd_dict[i.pali_1] = string
 
     dpd_dict = dict(
         sorted(dpd_dict.items(), key=lambda x: pali_sort_key(x[0])))
     print(f"{len(dpd_dict):,}")
 
-    # ---------------------------------------------------------------------
     # write dpd dict to json
-    # ---------------------------------------------------------------------
-
+    
     print(f"[green]{'writing dpd ebts to json':<40}", end="")
     with open(PTH.dpd_ebts_json_path, "w") as f:
         json.dump(dpd_dict, f, ensure_ascii=False, indent=0)
         print("OK")
 
-    # ---------------------------------------------------------------------
     # make a dict of all deconstructed compounds
-    # ---------------------------------------------------------------------
-
+    
     print(f"[green]{'making deconstructor dict':<40}", end="")
     deconstr_dict = {}
     for i in deconstr_db:
@@ -165,10 +144,8 @@ def main():
         sorted(deconstr_dict.items(), key=lambda x: pali_sort_key(x[0])))
     print(f"{len(deconstr_dict):,}")
 
-    # ---------------------------------------------------------------------
     # save deconstr dict to json
-    # ---------------------------------------------------------------------
-
+    
     print(f"[green]{'writing deconstructor dict to json':<40}", end="")
     with open(PTH.deconstructor_json_path, "w") as f:
         json.dump(deconstr_dict, f, ensure_ascii=False, indent=0)
