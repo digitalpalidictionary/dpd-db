cb39db1d985c012462ca47a5eebe9ad522af2752
bdhrs <bodhirasa@gmail.com>
2023-11-07 18:45:17 +0530

    more phonetic changes

--- tests/phonetic_changes.py
+++ tests/phonetic_changes.py
@@ -14,20 +14,13 @@ from db.get_db_session import get_db_session
 from db.models import PaliWord
 
 from tools.db_search_string import db_search_string
-from tools.meaning_construction import (
-    make_meaning, clean_construction)
+from tools.meaning_construction import clean_construction
 from tools.paths import ProjectPaths
 from tools.tsv_read_write import read_tsv_dot_dict
+from tools.pali_sort_key import pali_list_sorter
 
 
-def main():
-    pth = ProjectPaths()
-    csv = read_tsv_dot_dict(pth.phonetic_changes_path)
-
-    db_session = get_db_session(pth.dpd_db_path)
-    db = db_session.query(PaliWord).all()
-
-    pos_exclusions = ["sandhi", "idiom"]
+def add_update_phonetic(db_session, db, csv):
     
     for c_counter, c in enumerate(csv):
         if c.initial == "-":
@@ -37,7 +30,7 @@ def main():
         for k, v in c.items():
             print(f"[green]{str(k):<15}: [white]{str(v)}")
         print()
-        # exceptions = c.exceptions.split(", ")
+
         auto_updated = []
         auto_added = []
         manual_update = []
@@ -46,7 +39,6 @@ def main():
 
             if (
                 i.construction and
-                i.meaning_1 and
                 i.pali_1 not in c.exceptions):
 
                 # construction and base with:
@@ -57,17 +49,13 @@ def main():
                 base_clean = re.sub("√", "", base_clean)
 
                 # test construction
-                if (
-                    (
-                        c.initial in construction_clean or
-                        c.initial in base_clean
+                if ((c.initial in construction_clean or
+                     c.initial in base_clean
                     ) and
                     c.final in i.pali_clean and
                     c.correct not in i.phonetic and
-                    (
-                        c.without not in construction_clean and
-                        c.without not in base_clean
-                    )
+                    (c.without not in construction_clean and
+                     c.without not in base_clean)
                 ):
                     # auto update wrong to correct
                     if (
@@ -75,7 +63,7 @@ def main():
                         c.wrong in i.phonetic
                     ):
                         i.phonetic = re.sub(
-                            str(c.wrong), str(c.correct), str(i.phonetic))
+                            f"^{c.wrong}$", str(c.correct), str(i.phonetic))
                         auto_updated += [i.pali_1]
                     
                     # if i.phonetic empty > auto add correct
@@ -112,29 +100,66 @@ def main():
     db_session.close()
 
 
-def finder():
-    pth = ProjectPaths()
-    db_session = get_db_session(pth.dpd_db_path)
-    db = db_session.query(PaliWord).all()
+def finder(db, csv, string):
     found = []
 
     for i in db:
-        if (
-            "v > b" in str(i.phonetic) and
-            "īv" in str(i.phonetic)
-        ):
-            found += [i.pali_1]
-            i.phonetic = re.sub("v > b\n", "", str(i.phonetic))
-            i.phonetic = re.sub("\nv > b", "", str(i.phonetic))
+        for p in i.phonetic.split("\n"):
+            if string == p:
+                found += [i.pali_1]
     
-    found_string = db_search_string(found)
-    print(f"[green]{'found':<15}: [white]{found_string}")
-    pyperclip.copy(found_string)
+    if found:
+        found_string = db_search_string(found)
+        print(f"[cyan]{string}")
+        print(f"[white]{found_string}")
+        pyperclip.copy(found_string)
+    else:
+        print(None)
 
-    # db_session.commit()
 
+def list_all_phonetic_changes(db, csv):
+    # all phonetic changes minus all csv.correct
+    all_phonetic_set = set()
+    correct = set()
+
+    for c in csv:
+        if (
+            c.initial and
+            c.initial != "-"
+        ):
+            correct.update([c.correct])
+    
+    for i in db:
+        if i.phonetic:
+            phonetic = i.phonetic.split("\n")
+            all_phonetic_set.update(phonetic)
+    
+    all_phonetic_set = all_phonetic_set.difference(correct)
+    all_phonetic_list = pali_list_sorter(list(all_phonetic_set))
+    
+    # for p in all_phonetic_list:
+    #     print(p)
+    # print(len(all_phonetic_list))
+
+    with open("temp/phonetic_changes.txt", "w") as f:
+        for p in all_phonetic_list:
+            f.write(f"{p}\n")
+    
 
 
 if __name__ == "__main__":
-    main()
-    # finder()
+    pth = ProjectPaths()
+    csv = read_tsv_dot_dict(pth.phonetic_changes_path)
+
+    db_session = get_db_session(pth.dpd_db_path)
+    db = db_session.query(PaliWord).all()
+
+    list_all_phonetic_changes(db, csv)
+    add_update_phonetic(db_session, db, csv)
+    # finder(db, csv, "ṃs > s")
+
+# FIXME test for xyz not in pali
+# FIXME test for xyz not in base
+# FIXME test for xyz not in construction
+# FIXME exceptions must be a list
+# FIXME without must be a list
