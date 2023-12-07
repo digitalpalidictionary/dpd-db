1e60270a9d945d974dabedaea0da1b0836133812
Devamitta <devamitta@sasanarakkha.org>
2023-12-07 20:59:22 +0800

    add default values if missing

--- tools/configger.py
+++ tools/configger.py
@@ -9,46 +9,53 @@ from rich import print
 config = configparser.ConfigParser()
 config.read("config.ini")
 
+DEFAULT_CONFIG = {
+    "regenerate": {
+        "inflections": "yes",
+        "transliterations": "yes",
+        "freq_maps": "yes"
+    },
+    "deconstructor": {
+        "include_cloud": "no"
+    },
+    "gui": {
+        "theme": "DarkGrey10",
+        "screen_fraction_width": "0.60",
+        "screen_fraction_height": "1",
+        "window_x": "0",
+        "window_y": "0",
+        "font_name": "Noto Sans",
+        "font_size": "14",
+        "input_text_color": "darkgray",
+        "text_color": "#00bfff",
+        "element_padding_x": "0",
+        "element_padding_y": "0",
+        "margin_x": "0",
+        "margin_y": "0"
+    },
+    "goldendict": {
+        "copy_unzip": "yes",
+        "path": ""
+    },
+    "dictionary": {
+        "make_mdict": "yes",
+        "link_url": "https://www.thebuddhaswords.net/",
+        "make_link": "no"
+    },
+    "openia": {
+        "key": ""
+    }
+}
+
 
 def config_initialize() -> None:
-    """Initialize config.ini."""
-
-    config.add_section("regenerate")
-    config.set("regenerate", "inflections", "yes")
-    config.set("regenerate", "transliterations", "yes")
-    config.set("regenerate", "freq_maps", "yes")
-
-    config.add_section("deconstructor")
-    config.set("deconstructor", "include_cloud", "no")
-
-    config.add_section("gui")
-    config.set("gui", "theme", "DarkGrey10")
-    config.set("gui", "screen_fraction_width", "0.60")
-    config.set("gui", "screen_fraction_height", "1")
-    config.set("gui", "window_x", "0")
-    config.set("gui", "window_y", "0")
-    config.set("gui", "font_name", "Noto Sans")
-    config.set("gui", "font_size", "14")
-    config.set("gui", "input_text_color", "darkgray")
-    config.set("gui", "text_color", "#00bfff")
-    config.set("gui", "element_padding_x", "0")
-    config.set("gui", "element_padding_y", "0")
-    config.set("gui", "margin_x", "0")
-    config.set("gui", "margin_y", "0")
-
-    config.add_section("goldendict")
-    config.set("goldendict", "copy_unzip", "no")
-    config.set("goldendict", "path", "")
-
-    config.add_section("dictionary")
-    config.set("dictionary", "make_mdict", "yes")
-    config.set("dictionary", "link_url", "https://www.thebuddhaswords.net/")
-    config.set("dictionary", "make_link", "no")
-
-    config.add_section("openia")
-    config.set("openia", "key", "")
-    
-    config_write()
+    """Initialize config.ini with default values."""
+    for section, options in DEFAULT_CONFIG.items():
+        if not config.has_section(section):
+            config.add_section(section)
+        for option, value in options.items():
+            if not config.has_option(section, option):
+                config.set(section, option, value)
 
 
 def config_read(section: str, option: str, default_value=None) -> (str):
@@ -73,21 +80,27 @@ def config_update(section: str, option: str, value) -> None:
         config.add_section(section)
         config.set(section, option, str(value))
     config_write()
+    print(f"[green]config setting updated: {section}, {option}, {value}")
 
 
 def config_test(section: str, option: str, value) -> bool:
     """Test config.ini to see if a section, option equals a value."""
-    if (
-        section in config and
-        config.has_option(section, option)
-    ):
-        if config.get(section, option) == str(value):
-            return True
-        else:
-            return False
+    if config.has_section(section) and config.has_option(section, option):
+        return config.get(section, option) == str(value)
     else:
-        print(f"[red]unknown config setting: [brightyellow]{section}, {option}, {value}")
-        return False
+        print(f"[yellow]unknown config setting: [brightyellow]{section}, {option}, {value}")
+        
+        # Check if the section exists in the default config initialization
+        if section not in DEFAULT_CONFIG or option not in DEFAULT_CONFIG[section]:
+            print(f"[red]Missing default value for option: [brightyellow]{section}, {option}")
+            return False
+        
+        # If section or option missing, add default value for that section or option from DEFAULT_CONFIG
+        default_value = DEFAULT_CONFIG[section].get(option)
+        config_update(section, option, default_value)
+        
+        # Check if the value matches the provided value or the default value
+        return config.get(section, option) == str(value) or default_value == str(value)
 
 
 def config_test_section(section):
