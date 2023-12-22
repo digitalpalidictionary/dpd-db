e28800c5b1b115147b5313c732d6547a05e49453
bdhrs <bodhirasa@gmail.com>
2023-12-22 20:39:00 +0530

    additions

--- scripts/additions_add_to_db.py
+++ scripts/additions_add_to_db.py
@@ -9,77 +9,84 @@ from db.get_db_session import get_db_session
 from db.models import PaliWord
 
 from tools.paths import ProjectPaths
+from tools.addition_class import Addition
 
 
 def main():
+    print("[bright_yellow]add additions to db")
     pth = ProjectPaths()
     db_session = get_db_session(pth.dpd_db_path)
 
     # get the very last id number
     last_id = max(db_session.query(PaliWord.id).all())[0]
     next_id = last_id + 1
-    print(f"{'last_id:':<10}{last_id}")
-    print(f"{'next_id:':<10}{next_id}")
-
-    # save the old and new id's in a dict
-    id_dict = {}
+    print(f"{'last_id:':<20}{last_id:>10}")
+    print(f"{'next_id:':<20}{next_id:>10}")
 
     # open the additions pickle file
-    with open(pth.additions_pickle_path, "rb") as file:
-        additions_list = pickle.load(file)
-        
-        for a in additions_list:
-            # there are two pieces of data, a PaliWord and a comment
-            p, comment = a
-            print(f"{'comment:':<10}{comment}")
-            print(p)
-            
-            # id values
-            old_id = p.id
+    additions_list = Addition.load_additions()
+    
+    added_count = 0
+    for a in additions_list:
+        a : Addition
+        p = a.pali_word
+
+        # check if it's added
+        if not a.added_to_db:
+
+            # update the id
             p.id = next_id
             
-            # test if pali_1 exists or not
-            try:
-                test = db_session.query(PaliWord).filter(
-                    PaliWord.pali_1 == p.pali_1).one()
-                print(f"[red]{'error:':<10}pali_1 already exists in db")
-
-                # add a number if one doesnt exist
-                # or increment the number if it does
-                print(f"[red]{'':<10}incrementing number")
-
-                word = re.sub(" \\d.*", "", p.pali_1)
-                print(f"{'word:':<10}{word}")
-                digit = int(re.sub("(.+ )(\\d.*)", "\\2", p.pali_1))
-                if digit:
-                    new_digit = digit + 1
-                    print(f"{'digit:':<10}{digit}")
-                    p.pali_1 = f"{word} {new_digit}"
-                else:
-                    p.pali_1 = f"{word} 2"
-                print(p)  
-                 
-            except:
-                pass
-
-            # update the id dict
-            id_dict[old_id] = next_id
-            next_id = next_id + 1
-            db_session.add(p)
-            print()
+            # always use meaning_1
+            if not p.meaning_1:
+                p.meaning_1 = p.meaning_2
+            a.update(next_id)
             
-    
-    # db_session.commit()
-    print(f"{'added:':<10}{len(additions_list)}")
+            print(a)
 
-    # save the id_dict as a csv
-    # print(id_dict)
-    with open("temp/additions.tsv", "w") as tsv_file:
-        tsv_file.write("old_id\tnew_id\n")
-        for k,v in id_dict.items():
-            tsv_file.write(f"{k}\t{v}\n")
-    print(f"{'tsv_file:':<10}saved!")
+            # add it to the session
+            db_session.add(p)
+
+            # increment the id
+            next_id = next_id + 1
+            added_count += 1
+
+    Addition.save_additions(additions_list)
+
+    # db_session.commit()
+    print(f"{'added:':<20}{added_count:>10}")
+
+
+def convert_old_format():
+    new_additions_list = []
+
+    additions_list = Addition.load_additions()
+
+    for a in additions_list:
+        # there are two pieces of data, a PaliWord and a comment
+        pali_word, comment = a
+        if int(pali_word.id) >= 75577:
+            a = Addition(pali_word, comment, date_created="2023-12-01")
+            new_additions_list += [a]
+        
+    print(new_additions_list)
+    print(len(new_additions_list))
+    
+    Addition.save_additions(additions_list)
+                
+
+def update_old_id():
+    additions_list = Addition.load_additions()
+
+    for a in additions_list:
+        a.old_id = a.pali_word.id
+        print(a)
+
+    Addition.save_additions(additions_list)
+
 
 if __name__ == "__main__":
     main()
+    # convert_old_format()
+    # update_old_id()
 
---
+++ tools/addition_class.py
@@ -0,0 +1,73 @@
+import pickle
+
+from datetime import datetime
+from rich import print
+from typing import Optional
+
+from db.models import PaliWord
+from tools.paths import ProjectPaths
+
+pth = ProjectPaths()
+
+class Addition:
+    def __init__(
+            self,
+            pali_word: PaliWord,
+            comment: str,
+            date_created: Optional[str] = None,
+            old_id: Optional[int] = None,
+            new_id: Optional[int] = None,
+            added_to_db: bool = False,
+            added_date: Optional[str] = None
+    ):
+        self.pali_word = pali_word
+        self.comment = comment
+        if date_created is None:
+            self.date_created = datetime.now().strftime('%Y-%m-%d')
+        else:
+            self.date_created = date_created
+        self.old_id = pali_word.id
+        self.new_id = new_id
+        self.added_to_db = added_to_db
+        self.added_date = added_date
+
+    def update(
+            self,
+            new_id: int,
+            added_to_db: bool = True,
+            added_date: Optional[str] = None):
+        self.new_id = new_id
+        self.added_to_db = added_to_db
+        if added_date is None:
+            self.added_date = datetime.now().strftime('%Y-%m-%d')
+        else:
+            self.added_date = added_date
+
+    @ staticmethod
+    def load_additions():
+        with open(pth.additions_pickle_path, "rb") as file:
+            additions_list = pickle.load(file)
+        return additions_list
+
+    @ staticmethod
+    def save_additions(additions_list):
+        with open(pth.additions_pickle_path, "wb") as file:
+            pickle.dump(additions_list, file)
+
+    def __repr__(self):
+        return f"""
+{'pali_word':<20}{self.pali_word}
+{'comment':<20}{self.comment}
+{'date_created':<20}{self.date_created}
+{'old_id':<20}{self.old_id}
+{'new_id':<20}{self.new_id}
+{'added_to_db':<20}{self.added_to_db}
+{'added_date':<20}{self.added_date}
+"""
+
+if __name__ == "__main__":
+    pth = ProjectPaths()
+    additions_list = Addition.load_additions()
+    for a in additions_list:
+        print(a)
+
