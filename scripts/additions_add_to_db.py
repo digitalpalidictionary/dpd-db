4b932ad2a602bfde23ad13a18d63dd17ca115cce
bdhrs <bodhirasa@gmail.com>
2024-01-05 19:00:47 +0530

    additions

--- scripts/additions_add_to_db.py
+++ scripts/additions_add_to_db.py
@@ -1,8 +1,5 @@
 """Add user additions to db, saving the old & new IDs."""
 
-import pickle
-import re
-
 from rich import print
 
 from db.get_db_session import get_db_session
@@ -53,40 +50,9 @@ def main():
 
     Addition.save_additions(additions_list)
 
-    # db_session.commit()
+    db_session.commit()
     print(f"{'added:':<20}{added_count:>10}")
-
-
-def convert_old_format():
-    new_additions_list = []
-
-    additions_list = Addition.load_additions()
-
-    for a in additions_list:
-        # there are two pieces of data, a PaliWord and a comment
-        pali_word, comment = a
-        if int(pali_word.id) >= 75577:
-            a = Addition(pali_word, comment, date_created="2023-12-01")
-            new_additions_list += [a]
-        
-    print(new_additions_list)
-    print(len(new_additions_list))
-    
-    Addition.save_additions(additions_list)
-                
-
-def update_old_id():
-    additions_list = Addition.load_additions()
-
-    for a in additions_list:
-        a.old_id = a.pali_word.id
-        print(a)
-
-    Addition.save_additions(additions_list)
 
 
 if __name__ == "__main__":
     main()
-    # convert_old_format()
-    # update_old_id()
-
