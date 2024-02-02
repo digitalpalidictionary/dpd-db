c3bf0945e538b94d9b3b4dccf1322c79b167822e
bdhrs <bodhirasa@gmail.com>
2024-02-03 04:26:21 +0530

    sandhi contraction, update manual corrections

---
+++ scripts/sandhi_contraction_find_replace.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python3
+
+"""Find and replace sandhi contractions (imam'eva) 
+in example_1, example_2 and commentary."""
+
+import re
+import pyperclip
+
+from sqlalchemy import or_
+from rich import print
+
+from db.get_db_session import get_db_session
+from db.models import PaliWord
+
+from tools.db_search_string import db_search_string
+from tools.paths import ProjectPaths
+
+class ProgData():
+    def __init__(self, db_session) -> None:
+        self.db_session = db_session
+        self.find_me: str
+        self.replace_me: str
+
+    
+def main():
+    print("[bright_yellow]find and replace sandhi contractions")
+    pth = ProjectPaths()
+    db_session = get_db_session(pth.dpd_db_path)
+    pd = ProgData(db_session)
+    input_word(pd)
+
+
+def input_word(pd):
+    print("-"*50)
+    print()
+    print(f"[green]{'word to find':<40}", end="")
+    find_me = input()
+    if find_me == "x":
+        return
+    
+    pd.find_me = find_me
+
+    print(f"[green]{'word to replace':<40}", end="")
+    replace_me = input()
+    if replace_me == "x":
+        return
+    
+    pd.replace_me = replace_me
+   
+    print()
+    print(f"[green]replace [cyan]{find_me} [green]with [light_green]{replace_me}?")
+    print("[white]y[green]es, [white]n[green]o, search in [white]b[green]old, e[white]x[green]it ", end="")
+    route = input()
+    print()
+    if route == "n":
+        input_word(pd)
+    elif route == "b":
+        find_instances_in_bold(pd)
+    elif route == "x":
+        return
+    else:
+        find_instances(pd)
+
+
+def find_instances(pd):
+    db = pd.db_session.query(PaliWord) \
+        .filter(
+            or_(
+                PaliWord.example_1.contains(pd.find_me),
+                PaliWord.example_2.contains(pd.find_me),
+                PaliWord.commentary.contains(pd.find_me))
+                ).all()
+   
+    pd.db = db
+
+    counter = 0
+    for i in pd.db:
+        for column in ["example_1", "example_2", "commentary"]:
+            field = getattr(i, column)
+            if field.find(pd.find_me) != -1:
+                counter += 1
+    
+    print(f"{counter} instance(s) found")
+    print()
+
+    if counter != 0:
+        replace_instances(pd)
+    else:
+        find_instances_in_bold(pd)
+
+
+def find_instances_in_bold(pd):
+    db = pd.db_session.query(PaliWord).all()
+    pd.db = db
+
+    print("[green]searching inside bold strings")
+    counter = 0
+
+    found_list = []
+    for i in pd.db:
+        for field in ["example_1", "example_2", "commentary"]:
+            clean_field = getattr(i, field) \
+                .replace("<b>", "") \
+                .replace("</b>", "")
+            if clean_field.find(pd.find_me) != -1:
+                counter += 1
+                found_list += [(i.pali_1, field)]
+
+    print(f"{counter} instance(s) found")
+    print()
+
+    if counter > 0:
+        print("[green]replace the following fields manually")
+        for i in found_list:
+            print(f"[green]{i[0]:<30}[cyan]{i[1]:<20}[white]{pd.find_me}")
+    
+    db_search = db_search_string([i[0] for i in found_list])
+    pyperclip.copy(db_search)
+    print()
+    print("[green]db search string copied to clipboard")
+    print(f"[white]{db_search}")
+    print("[green]press any key to continue ", end="")
+    input()
+    print()
+
+    input_word(pd)
+    
+
+def replace_instances(pd):
+    counter = 0
+    route = None
+
+    for i in pd.db:
+        if route == "x":      
+            break
+
+        for column in ["example_1", "example_2", "commentary"]:
+            field = getattr(i, column)
+            if field.find(pd.find_me) != -1:
+                counter += 1
+
+                print(f"[green]{counter}. {i.pali_1}: {column}")
+                print()
+                
+                print("[yellow]before")
+                example_before = field.replace(
+                    pd.find_me,
+                    f"[cyan]{pd.find_me}[/cyan]")
+                print(example_before)
+                print()
+                
+                print("[yellow]after")
+                example_after = field.replace(
+                    pd.find_me,
+                    f"[light_green]{pd.replace_me}[/light_green]")
+                print(example_after)
+                print()
+
+                print("[green]press [white]c[green]ommit or any key to continue ", end="")
+                route = input()
+                
+                if route == "c":
+                    setattr(
+                        i,
+                        column, 
+                        re.sub(pd.find_me, pd.replace_me, field)
+                    )
+                    try:
+                        pd.db_session.commit()
+                        print(f"[cyan]{pd.find_me}[green] > [light_green]{pd.replace_me} [green]committed to db")
+                        print()
+                    except Exception as e:
+                        print(f"[red]{e}")
+                        replace_instances(pd)
+
+
+                    
+                
+                elif route == "x":
+                    break
+                
+    input_word(pd)
+            
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+# unused
+def replace_instances_regex(pd):
+    counter = 0
+    route = None
+
+    for i in pd.db:
+        if route == "x":  
+            break
+
+        for column in ["example_1", "example_2", "commentary"]:
+            field = getattr(i, column)
+            if re.findall(pd.find_me, field):
+                counter += 1
+
+                print()
+                print(f"[green]{counter}. {i.pali_1}: {column}")
+                print()
+                
+                print("[yellow]before")
+                example_before = re.sub(
+                    pd.find_me,
+                    f"[cyan]{pd.find_me}[/cyan]",
+                    field)
+                print(example_before)
+                print()
+                
+                print("[yellow]after")
+                example_after = re.sub(
+                    pd.find_me,
+                    f"[light_green]{pd.replace_me}[/light_green]",
+                    field)
+                print(example_after)
+                print()
+                print("[green]press [white]c[green]ommit or any key to continue ", end="")
+                route = input()
+                print()
+
+                if route == "c":
+                    setattr(
+                        i,
+                        column, 
+                        re.sub(pd.find_me, pd.replace_me, field)
+                    )
+                    pd.db_session.commit()
+                    print(f"[cyan]{pd.find_me}[green] > [light_green]{pd.replace_me} [green]committed to db")
+                    print()
+                
+                elif route == "x":
+                    break
+            
+                
+    input_word(pd)
---
+++ tests/deconstructor_combine_db_and_manual_corrections.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+"""Combine all the sandhi compounds in db with manual corrections,
+so that they are all recognised in deconstructor."""
+
+from rich import print
+
+from db.get_db_session import get_db_session
+from db.models import PaliWord
+
+from tools.meaning_construction import clean_construction
+from tools.paths import ProjectPaths
+from tools.tsv_read_write import read_tsv
+
+def main():
+    pth = ProjectPaths()
+    db_session = get_db_session(pth.dpd_db_path)
+    db = db_session.query(PaliWord).all()
+    
+    sandhi_dict = {}
+    sandhi_dupes = []
+    for i in db:
+        if i.pos == "sandhi" and i.meaning_1:
+            constr_clean = clean_construction(i.construction).strip()
+            if i.pali_clean not in sandhi_dict:
+                sandhi_dict[i.pali_clean] = [constr_clean]
+            else:
+                if constr_clean not in sandhi_dict[i.pali_clean]:
+                    sandhi_dict[i.pali_clean] += [constr_clean]
+                else:
+                    sandhi_dupes += [i.pali_clean]
+
+    print(len(sandhi_dict))
+
+    manual_data = read_tsv(pth.manual_corrections_path)
+    manual_dict = {}
+    manual_dupes = []
+    for i in manual_data:
+        word, breakup = i
+        breakup = breakup.strip()
+        if word not in manual_dict:
+            manual_dict[word] = [breakup]
+        else:
+            if breakup not in manual_dict[word]:
+                manual_dict[word] += [breakup]
+            else:
+                manual_dupes += [word]
+    print(len(manual_dict))
+
+    new_dict = {}
+    new_dupes = []
+    for word, breakups in manual_dict.items():
+        for breakup in breakups:
+            if word not in new_dict:
+                new_dict[word] = [breakup]
+            else:
+                if breakup not in new_dict[word]:
+                    new_dict[word] += [breakup]                      
+                else:
+                    new_dupes += [word]
+
+    for word, breakups in sandhi_dict.items():
+        for breakup in breakups:
+            if word not in new_dict:
+                new_dict[word] = [breakup]
+            else:
+                if breakup not in new_dict[word]:
+                    new_dict[word] += [breakup]                      
+                else:
+                    new_dupes += [word]
+
+    print(len(new_dict))
+
+    with open(pth.manual_corrections_path, "w") as file:
+        for word, breakups in new_dict.items():
+            for breakup in breakups:
+                file.write(f"{word}\t{breakup}\n")
+
+if __name__ == "__main__":
+    main()
