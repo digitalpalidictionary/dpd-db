c4b97ab20f03d573c3ef22f8bd0430dddaf2824e
bdhrs <bodhirasa@gmail.com>
2024-04-03 08:43:40 +0530

    draft release

--- scripts/summary.py
+++ scripts/summary.py
@@ -47,7 +47,21 @@ def main():
         print()
         print(f"new words include: {new_words_string}")
 
-        summary_text = f"""**Change-log**:\\n{line1}\\n{line2}\\n{line3}\\n{line4}\\n{line5}\\n{line6}\\n- 100% dictionary recognition in the early texts\\n- xyz\\n- numerous additions and corrections based on user feedback\\n\\n**new words include**: {new_words_string}"""
+        summary_text = f"""
+**Change-log**:
+{line1}
+{line2}
+{line3}
+{line4}
+{line5}
+{line6}
+- 100% dictionary recognition in the early texts
+- xyz
+- numerous additions and corrections based on user feedback
+
+**new words include**: {new_words_string}
+"""
+        
         with open(pth.summary_md_path, "w") as f:
             f.write(summary_text)
 
