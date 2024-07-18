8871cbd4117cf0120dd6b3b8d8d488c6c89f723c
Khemarato Bhikkhu <khemarato.bhikkhu@gmail.com>
2024-07-18 13:19:36 +0700

    Read initial q param

--- exporter/dpd_fastapi/static/home.js
+++ exporter/dpd_fastapi/static/home.js
@@ -33,6 +33,56 @@ var fontSizeDisplay = document.getElementById("font-size-display");
 
 let dpdResultsContent = "";
 
+//// uri utils
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+      }
+    }
+}
+
+function updateQueryString(key, value, url) {
+    value = encodeURIComponent(value);
+    if (!url) url = window.location.href;
+    var re = new RegExp("([?&])" + key + "=.*?(&|#|$)(.*)", "gi"),
+        hash;
+
+    if (re.test(url)) {
+        if (typeof value !== 'undefined' && value !== null) {
+            return url.replace(re, '$1' + key + "=" + value + '$2$3');
+        } 
+        else {
+            hash = url.split('#');
+            url = hash[0].replace(re, '$1$3').replace(/(&|\?)$/, '');
+            if (typeof hash[1] !== 'undefined' && hash[1] !== null) {
+                url += '#' + hash[1];
+            }
+            return url;
+        }
+    }
+    else {
+        if (typeof value !== 'undefined' && value !== null) {
+            var separator = url.indexOf('?') !== -1 ? '&' : '?';
+            hash = url.split('#');
+            url = hash[0] + separator + key + '=' + value;
+            if (typeof hash[1] !== 'undefined' && hash[1] !== null) {
+                url += '#' + hash[1];
+            }
+            return url;
+        }
+        else {
+            return url;
+        }
+    }
+}
+
 //// load state
 
 function loadToggleState(id) {
@@ -83,7 +133,8 @@ document.addEventListener("DOMContentLoaded", function() {
     swopSansSerif()
     if (dpdResults.innerHTML.trim() == "") {
         dpdResults.innerHTML = startMessage
-    } 
+    }
+    applyUrlQuery();
 });
 
 //// listeners
@@ -240,6 +291,15 @@ async function handleFormSubmit(event) {
             console.error("Error fetching data:", error);
         }
     }
+}
+
+//// url query param
+
+function applyUrlQuery() {
+    const query = getQueryVariable('q');
+    if(!query) return;
+    searchBox.value = query;
+    handleFormSubmit().then();
 }
 
 //// populate history
