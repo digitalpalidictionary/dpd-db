2910c18c0fdbfefe800f0d15c0914289b3b2c1d4
Devamitta Bhikkhu <39419221+Devamitta@users.noreply.github.com>
2024-10-09 06:41:07 +0800

    Clean bash

--- scripts/bash/build_db_and_export_all.sh
+++ scripts/bash/build_db_and_export_all.sh
@@ -3,12 +3,7 @@
 
 scripts/bash/initial_setup_run_once.sh
 
-set -e
-test -e dpd.db || touch dpd.db
-
-scripts/db_rebuild_from_tsv.py
-
-db/bold_definitions/update_bold_definitions_db.py
+scripts/bash/build_db.sh
 
 scripts/bash/makedict.sh
 
