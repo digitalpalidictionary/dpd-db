98b3b2f3f48e122b4c6cd8b922d6ee9594c2774c
bdhrs <bodhirasa@gmail.com>
2024-11-17 18:52:00 +0530

    test hyphenations update

--- db_tests/test_hyphenations.py
+++ db_tests/test_hyphenations.py
@@ -4,6 +4,7 @@
 
 import json
 import re
+import subprocess
 import pyperclip
 
 from rich import print
@@ -16,10 +17,15 @@ from tools.pali_alphabet import pali_alphabet
 from tools.tic_toc import tic, toc
 from tools.db_search_string import db_search_string
 
+max_length = 18
+only_do_hyphenated_words = True
+# True max_length =  18
+# False max_length = 30
 
-class ProgData():
+
+class GlobalVars():
     def __init__(self) -> None:
-        self.max_length: int = 30
+        self.max_length: int = max_length
         
         self.pth = ProjectPaths()
         self.db_session = get_db_session(self.pth.dpd_db_path)
@@ -27,12 +33,14 @@ class ProgData():
         self.hyphenations_dict: dict[str, str] = self.load_hyphenations_dict()
         
         self.clean_words_dict: dict
+        self.hyphenated_words_dict: dict
         self.long_words_dict: dict
         self.variations_dict: dict
 
         self.ids: Optional[set] = None
         self.spelling_chosen: Optional[str] = None
         self.spelling_other: Optional[list[str]] = None
+        self.spelling_clean: str = ""
         
         self.yes_no: str = "[white]y[green]es [white]n[green]o "
         
@@ -60,15 +68,15 @@ class ProgData():
         self.save_hyphenations_dict()
  
     
-def extract_clean_words(pd):
+def extract_clean_words(g: GlobalVars):
     """extract all clean words from the db in examples and commentary."""
-    print("[green]exatrcting words from db")
+    print("[green]extracting words from db")
 
     # compile regex of pali_alphabet space apostrophe dash 
     regex_compile = re.compile(f"[^{'|'.join(pali_alphabet)}| |'|-]")
     
     clean_words_dict: dict = {}
-    for i in pd.db:
+    for i in g.db:
         id = i.id
         for column in ["example_1", "example_2", "commentary"]:
             cell = getattr(i, column)
@@ -83,22 +91,34 @@ def extract_clean_words(pd):
                     else:
                         clean_words_dict[clean_word].add(id)
 
-    pd.clean_words_dict = clean_words_dict
+    g.clean_words_dict = clean_words_dict
 
 
-def find_long_words(pd):
+def find_long_words(g: GlobalVars):
     """Find all the long words"""
     print("[green]finding long words")
     
     long_words_dict: dict = {}
-    for word, ids in pd.clean_words_dict.items():
-        if len(word) > pd.max_length:
+    for word, ids in g.clean_words_dict.items():
+        if len(word) > g.max_length:
             long_words_dict.update({word: ids})
     
-    pd.long_words_dict = long_words_dict
+    g.long_words_dict = long_words_dict
 
 
-def find_variations(pd):
+def find_hyphenated_words(g: GlobalVars):
+    """Find all hyphenated words"""
+
+    print("[green]finding hyphenated words")
+    hyphenated_words_dict: dict = {}
+    for word, ids in g.clean_words_dict.items():
+        if "-" in word:
+            hyphenated_words_dict.update({word: ids})
+    
+    g.hyphenated_words_dict = hyphenated_words_dict
+
+
+def find_variations(g: GlobalVars):
     """Find apostrophe and hyphenation variations in long words."""
     print("[green]finding apostrophe and hyphenation variations")
     
@@ -106,13 +126,13 @@ def find_variations(pd):
     regex_compile = re.compile(f"[^{'|'.join(pali_alphabet)}]")
     
     variations_dict: dict = {}
-    for dirty_word, ids in pd.long_words_dict.items():
+    for dirty_word, ids in g.long_words_dict.items():
         clean_word = re.sub(regex_compile, "", dirty_word)
 
         # test it's not a known hyphenation
         if (
-            clean_word not in pd.hyphenations_dict
-            and pd.hyphenations_dict.get(clean_word, None) != dirty_word
+            clean_word not in g.hyphenations_dict
+            and g.hyphenations_dict.get(clean_word, None) != dirty_word
         ):
             # add to or update variations_dict
             if clean_word not in variations_dict:
@@ -125,29 +145,44 @@ def find_variations(pd):
     for key, values in variations_dict.items():
             variations_dict[key]["dirty_words"] = list(set(values["dirty_words"]))
 
-    pd.variations_dict = variations_dict
+    g.variations_dict = variations_dict
 
 
 # FIXME 
 # what about the case where the hyphenation differs from the saved version??
 
 
-def process_long_words(pd):
+def has_hyphenated_word(dirty_words):
+    for word in dirty_words:
+        if "-" in word:
+            return True
+    return False
+
+
+def process_long_words(g: GlobalVars):
     """process long words without hyphenation or with multiple versions"""
     print("[green]showing clean words and dirty variations")
 
-    for counter, (clean_word, values) in enumerate(pd.variations_dict.items()):
+    # for counter, (clean_word, values) in enumerate(g.variations_dict.items()):
+    for counter, (clean_word, values) in enumerate(g.variations_dict.items()):
         dirty_words = values["dirty_words"]
-        pd.ids = values["ids"]
+        g.ids = values["ids"]
         
-        if clean_word not in pd.hyphenations_dict:
+        if clean_word not in g.hyphenations_dict:
+
+            if (
+                only_do_hyphenated_words 
+                and not has_hyphenated_word(dirty_words)
+            ):
+                continue
+
             print("_"*50)
             print()
-            print(f"{counter+1} / {len(pd.variations_dict)}")
+            print(f"{counter+1} / {len(g.variations_dict)}")
             print(f"[green]{clean_word} [blue]{len(clean_word)}")
             
-            pd.spelling_chosen = None
-            pd.spelling_other = None
+            g.spelling_chosen = None
+            g.spelling_other = None
             
             for dcount, dirty_word in enumerate(dirty_words):
                 print(f"{dcount+1} [light_green]{dirty_word} [blue]{len(dirty_word)}")
@@ -157,21 +192,21 @@ def process_long_words(pd):
             choice = input()
             print()
             
-            if choice.isdigit():
+            if choice.isdigit() and dcount > 0:
                 chosen_word = dirty_words[int(choice)-1]
                 
-                pd.spelling_clean = clean_word
-                pd.spelling_chosen = chosen_word
-                pd.spelling_other = [clean_word]
-                pd.spelling_other.extend(dirty_words)
-                pd.spelling_other.remove(chosen_word)
-                pd.spelling_other = list(set(pd.spelling_other))
-                replace_word_in_db(pd)
+                g.spelling_clean = clean_word
+                g.spelling_chosen = chosen_word
+                g.spelling_other = [clean_word]
+                g.spelling_other.extend(dirty_words)
+                g.spelling_other.remove(chosen_word)
+                g.spelling_other = list(set(g.spelling_other))
+                replace_word_in_db(g)
             
             elif choice == "m":
                 dirty_words_str = '\n'.join(dirty_words)
                 
-                with open(pd.pth.hyphenations_scratchpad_path, "w") as file:
+                with open(g.pth.hyphenations_scratchpad_path, "w") as file:
                     file.write(f"{clean_word}\n{dirty_words_str}")                                
                 pyperclip.copy(f"{clean_word}\n{dirty_words_str}")
                 
@@ -183,12 +218,12 @@ def process_long_words(pd):
                     chosen_word_clean = chosen_word.replace("-", "").replace("'", "")
                     
                     if chosen_word_clean == clean_word:
-                        pd.spelling_clean = clean_word
-                        pd.spelling_chosen = chosen_word
-                        pd.spelling_other = [clean_word]
-                        pd.spelling_other.extend(dirty_words)
-                        pd.spelling_other = list(set(pd.spelling_other))
-                        replace_word_in_db(pd)
+                        g.spelling_clean = clean_word
+                        g.spelling_chosen = chosen_word
+                        g.spelling_other = [clean_word]
+                        g.spelling_other.extend(dirty_words)
+                        g.spelling_other = list(set(g.spelling_other))
+                        replace_word_in_db(g)
                     
                     else:
                         print("[red]spelling error, better luck next time")
@@ -197,8 +232,11 @@ def process_long_words(pd):
                 else:
                     print("[red]nothing pasted")
             
-            elif choice == "o":
-                pd.update_hyphenations_dict(clean_word, dirty_words[0])
+            elif (
+                choice == "o"
+                or choice.isdigit() and dcount == 0
+            ):
+                g.update_hyphenations_dict(clean_word, dirty_words[0])
                 print("[white]test_hyphenations.json [green]updated")
             
             elif choice == "x":
@@ -211,22 +249,21 @@ def process_long_words(pd):
             print(f"[red]{clean_word}[/red] already in [white]test_hyphenations.json")
 
 
-
-def replace_word_in_db(pd):
+def replace_word_in_db(g: GlobalVars):
     """Replace all variations with the preferred hyphenation."""
 
     replacement_count = 0
     db_list = []
-    for replace_me in pd.spelling_other:
-        db = pd.db_session.query(DpdHeadword)\
+    for replace_me in g.spelling_other:
+        db = g.db_session.query(DpdHeadword)\
             .filter(DpdHeadword.example_1.contains(replace_me)).all()
         db_list.extend(db)
         
-        db = pd.db_session.query(DpdHeadword)\
+        db = g.db_session.query(DpdHeadword)\
             .filter(DpdHeadword.example_2.contains(replace_me)).all()
         db_list.extend(db)
         
-        db = pd.db_session.query(DpdHeadword)\
+        db = g.db_session.query(DpdHeadword)\
             .filter(DpdHeadword.commentary.contains(replace_me)).all()
         db_list.extend(db)
 
@@ -239,13 +276,13 @@ def replace_word_in_db(pd):
             ]:
 
                 if replace_me in field:
-                    field_new = field.replace(replace_me, pd.spelling_chosen)
+                    field_new = field.replace(replace_me, g.spelling_chosen)
                     
                     field_old_highlight = field.replace(replace_me, f'[cyan]{replace_me}[/cyan]')
                     print(f"[green]{field_old_highlight}")
                     print()
                     
-                    field_new_highlight = field_new.replace(pd.spelling_chosen, f"[cyan]{pd.spelling_chosen}[/cyan]")
+                    field_new_highlight = field_new.replace(g.spelling_chosen, f"[cyan]{g.spelling_chosen}[/cyan]")
                     print(f"[light_green]{field_new_highlight}")
                     print()
 
@@ -255,13 +292,13 @@ def replace_word_in_db(pd):
     print(f"[green]{replacement_count} replacement(s) made")
     
     if replacement_count > 0:
-        pd.db_session.commit()
-        pd.update_hyphenations_dict(pd.spelling_clean,pd.spelling_chosen)
+        g.db_session.commit()
+        g.update_hyphenations_dict(g.spelling_clean,g.spelling_chosen)
         print("[green]committed to db and updated [light_green]test_hyphenations.json")
     
     else:
         
-        ids_str = db_search_string(pd.ids)
+        ids_str = db_search_string(g.ids)
         pyperclip.copy(ids_str)
         print("[green]try replacing manually, the db id's are copied to the clipboard")
         print("[green]press any key to continue ", end="")
@@ -269,21 +306,23 @@ def replace_word_in_db(pd):
 
 
 def main():
+
     tic()
     print("[bright_yellow]testing and correcting hyphenation of long words")
-    print()
-    pd = ProgData()
+    
+    g = GlobalVars()
 
-    # subprocess.Popen(
-    #     ["code", pd.pth.hyphenations_dict_path])
+    subprocess.Popen(
+        ["code", g.pth.hyphenations_dict_path])
+    subprocess.Popen(
+        ["code", g.pth.hyphenations_scratchpad_path]) 
 
-    # subprocess.Popen(
-    #     ["code", pd.pth.hyphenations_scratchpad_path]) 
-
-    extract_clean_words(pd)
-    find_long_words(pd)
-    find_variations(pd)
-    process_long_words(pd)
+    extract_clean_words(g)
+    find_long_words(g)
+    find_hyphenated_words(g)
+    find_variations(g)
+    process_long_words(g)
+    
     toc()
 
 
