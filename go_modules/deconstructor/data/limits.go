b0a6dc2fda333264598ef760348a1db524b836be
bdhrs <bodhirasa@gmail.com>
2024-11-17 18:52:53 +0530

    deconstructor limits

--- go_modules/deconstructor/data/limits.go
+++ go_modules/deconstructor/data/limits.go
@@ -22,12 +22,12 @@ func InitLimits() Limits {
 	l.WordLimit = 0            // 0
 	l.CountEvery = 25000       // 10000 - 50000
 	l.MaxWordLength = 0        // 0
-	l.MaxProcessesSingle = 15  // 15-20
-	l.MaxProcessesTotal = 1500 // 1000-2000
+	l.MaxProcessesSingle = 20  // 15-20
+	l.MaxProcessesTotal = 2000 // 1000-2000
 	l.LwCleanListMaxLen = 5    // 5
 	l.LwCleanMinLen = 4        // 4
 	l.LwFuzzyListMaxLen = 5    // 5
 	l.LwFuzzyMinLen = 3        // 3
-	l.TopDictLimit = 4         // 5 entries in GoldenDict
+	l.TopDictLimit = 5         // 5 entries in GoldenDict
 	return l
 }
