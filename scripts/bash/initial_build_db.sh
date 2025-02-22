2e40e8058e132eab1e97d845ab2cc07950299857
bdhrs <bodhirasa@gmail.com>
2025-02-22 16:22:55 +0530

    bash cl scripts

--- scripts/bash/initial_build_db.sh
+++ scripts/bash/initial_build_db.sh
@@ -5,11 +5,11 @@
 set -e
 test -e dpd.db || touch dpd.db
 
-scripts/build/db_rebuild_from_tsv.py
+uv run scripts/build/db_rebuild_from_tsv.py
 
-db/bold_definitions/update_bold_definitions_db.py
+uv run db/bold_definitions/update_bold_definitions_db.py
 
-scripts/bash/generate_components.sh
+uv run scripts/bash/generate_components.sh
 
 # After setting db_rebuild to "yes" in db_rebuild_from_tsv.py, we change it back after the bash is done.
 python -c "from tools.configger import config_update; config_update('regenerate', 'db_rebuild', 'no')"
