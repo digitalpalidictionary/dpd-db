ed8c9db77c95e3ff8a6d9e5de403bf324aa023ec
bdhrs <bodhirasa@gmail.com>
2025-03-12 01:49:56 +0530

    docs: using db, db_search_example

--- docs/use_db.md
+++ docs/use_db.md
@@ -1,5 +1,7 @@
 ## Using the DB
 
+There is a comprehensive database model and lots of pre-existing tools in the repo. To use them:
+
 1. Clone this repo
 2. Download **dpd.db.tar.bz2** from [this page](https://github.com/digitalpalidictionary/dpd-db/releases), 
 3. Unzip and place the db in the root of the project folder
@@ -11,4 +13,6 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 ```bash
 uv sync
 ```
-6. See `scripts/tutorial/db_search_example.py` for a quick tutorial on how to use the database with SQLAlchemy
+6. See [`scripts/tutorial/db_search_example.py`](../scripts/tutorial/db_search_example.py) for a quick tutorial on how to use the database with SQLAlchemy
+
+Alternatively, just download the db and use it with your tools of choice. 
--- scripts/tutorial/db_search_example.py
+++ scripts/tutorial/db_search_example.py
@@ -1,63 +1,66 @@
-"""A commented tutorial on how to search the db using SQLAlechmy,
-make changes and commit them to the database."""
+"""A tutorial demonstrating how to search the DPD database using SQLAlchemy,
+and how to make changes and commit them to the database."""
 
-# these are the imports you will always need
+# These are the essential imports needed for database operations
 from db.db_helpers import get_db_session
 from tools.paths import ProjectPaths
 
-# if you want to access any table, import its Class here.
-# other options are DpdRoot, Sandhi, families etc.
+# To access any table, import its corresponding class here
+# Other available options include DpdRoot, DbInfo, and Lookup, etc.
 from db.models import DpdHeadword
 
 pth = ProjectPaths()
-# this connects to the db, use it once to access the db
+# This establishes a connection to the database - use it once to access the database
 db_session = get_db_session(pth.dpd_db_path)
 
-# this is how to search a table using a basic filter for
-# pos == "adj" and words starts with "abh"
-search_results = db_session.query(DpdHeadword).filter(
-    DpdHeadword.pos == "adj", DpdHeadword.lemma_1.startswith("a")).all()
-# take note the results are returned as a [list] of DpdHeadword class instances
+# Example of searching a table using basic filters:
+# Find records where part of speech is "adj" and words start with "a"
+search_results = (
+    db_session.query(DpdHeadword)
+    .filter(DpdHeadword.pos == "adj", DpdHeadword.lemma_1.startswith("a"))
+    .all()
+)
+# Note: The results are returned as a list of DpdHeadword class instances
 
-# then loop through the list of results
+# Loop through the list of search results
 for i in search_results:
-
-    # now you can access any table column by dot notation
+    # Access any table column using dot notation
     print(f"{'PALI:':<15}{i.lemma_1}")
-    # :<15 means 'left justify the text by 15 characters'
+    # The :<15 format specifier left-justifies the text within 15 characters
     print(f"{'POS:':<15}{i.pos}")
 
-    # show meaning_2 if meaning_1 is empty
+    # Display meaning_2 if meaning_1 is empty
     if i.meaning_1:
         print(f"{'MEANING:':<15}{i.meaning_1}")
     else:
         print(f"{'MEANING:':<15}{i.meaning_2}")
 
-    # this checks if there's a root
+    # Check if there is a root associated with the word
     if i.root_key:
-        # if you don't do this, you will get a NoneType Attribute Error
-        # trying to access root information where there is no root
+        # This check prevents a NoneType AttributeError when trying to access
+        # root information for words without an associated root
 
-        # now you can access the root table using i.rt.root column names
+        # Access the root table using i.rt.root column names
         print(
-            f"{'ROOT':<15}{i.rt.root_clean} {i.rt.root_group} {i.root_sign} ({i.rt.root_meaning})")
+            f"{'ROOT':<15}{i.rt.root_clean} {i.rt.root_group} {i.root_sign} ({i.rt.root_meaning})"
+        )
 
-    # this imports the inflections list which is stored as csv list
+    # Import the inflections list which is stored as a CSV-formatted string
     inflections = i.inflections_list
 
-    # this loops through the inflections and prints them out
+    # Loop through the inflections and print them
     print(f"{'INFLECTIONS:':<15}", end="")
     for inflection in inflections:
         print(f"{inflection}", end=" ")
     print()
     print()
 
-    # you could edit any information in the db now, for example
+    # You can modify any database information here, for example:
     # if i.pos == "adj":
     #     i.pos = "adjective"
 
-# this would commit any changes you make to the db - remember there is no undo!
+# This would commit any changes to the database - remember there is no undo!
 # db_session.commit()
 
-# and always close the db session once you're done'
+# Always close the database session when you're finished
 db_session.close()
