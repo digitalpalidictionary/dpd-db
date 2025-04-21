dae6e00b9fc7c2832b46700b8aa2fa9d6a67670a
bdhrs <bodhirasa@gmail.com>
2025-04-21 20:45:31 +0530

    audio: save settings with test mp3 #98

--- db/audio/generate_audio.py
+++ db/audio/generate_audio.py
@@ -3,8 +3,10 @@
 """Find the most frequent words and generate audio files using ElevenLabs."""
 
 from dataclasses import dataclass, field
+from json import dump
 from pathlib import Path
 import traceback
+import time
 
 from aksharamukha import transliterate
 from elevenlabs import VoiceSettings
@@ -26,7 +28,6 @@ class VoxItem:
     lemma_1s: list[str] = field(default_factory=list)
     ebt_count: int = 0
     devanagari: str = ""
-    file_name: Path = Path()
 
 
 class VoxManager:
@@ -40,9 +41,6 @@ class VoxManager:
             .all()
         )
 
-        self.output_dir: Path = Path("db/audio/mp3")
-        self.output_dir.mkdir(parents=True, exist_ok=True)
-
         self.vox_unprocessed: dict[str, VoxItem] = {}
         self.make_vi_dict()
 
@@ -55,7 +53,6 @@ class VoxManager:
                 vox.lemma_1s = [i.lemma_1]
                 vox.ebt_count = i.ebt_count
                 vox.devanagari = self.convert_to_devanagari(i.lemma_clean)
-                vox.file_name = self.output_dir / f"{i.lemma_clean}.mp3"
             else:
                 self.vox_unprocessed[i.lemma_clean].lemma_1s.append(i.lemma_1)
 
@@ -65,7 +62,8 @@ class VoxManager:
             "Devanagari",
             text,
             nativize=True,
-            post_options=["devanagariuttara", "ShowSchwaHindi"],
+            # post_options=["devanagariuttara", "ShowSchwaHindi"],
+            post_options=["ShowSchwaHindi"],
         )
         if text_trans:
             return text_trans
@@ -74,28 +72,67 @@ class VoxManager:
 
 
 class ElevenLabsManager:
-    api_key = config_read("apis", "eleven_labs")
-    client = ElevenLabs(
-        api_key=api_key,
-    )
+    def __init__(self):
+        # --- API ---
+        self.api_key = config_read("apis", "eleven_labs")
+        self.client = ElevenLabs(api_key=self.api_key)
 
-    voices = {
-        "kanika": "XcWoPxj7pwnIgM3dQnWv",
-        "brian": "nPczCjzI2devNBz1zQrb",
-    }
-    voice = "kanika"
-    voice_id = voices[voice]
-    output_format = "mp3_44100_128"
-    text: str
-    model_id: str = "eleven_multilingual_v2"
-    overwrite: bool = False
+        # --- File ---
+        self.output_dir: Path = Path("db/audio/mp3")
+        self.output_dir.mkdir(parents=True, exist_ok=True)
+
+        self.voices = {
+            "kanika": "XcWoPxj7pwnIgM3dQnWv",
+            "brian": "nPczCjzI2devNBz1zQrb",
+            "viraj": "1U02n4nD6AdIZ9CjF053",
+            "kishan": "37frHvUllvzviJDpT2Qa",
+            "amit": "Sxk6njaoa7XLsAFT7WcN",
+            "netra": "nsuWityFzyjklsOeHYMt",
+            "shanaya": "nsuWityFzyjklsOeHYMt",
+            "zara": "ADd2WEtjmwokqUr0Y5Ad",
+            "aaditya": "xMagNCpMgZ83QOEsHNre",
+            "god": "PLFXYRTU74HpuNdj6oDl",
+            "ruan": "IFINFMv1Samh9a9fDnWQ",
+        }
+        self.voice = "ruan"
+        self.voice_id = self.voices[self.voice]
+        self.output_format = "mp3_44100_128"
+        self.model_id: str = "eleven_multilingual_v2"
+        self.overwrite: bool = False
+
+        # --- Voice Settings ---
+        self.stability = 0.9
+        self.similarity_boost = 1
+        self.style = 0.1
+        self.use_speaker_boost = False
+        self.speed = 0.85
+
+        self.sleep_time = 2  # seconds
+
+    def save_settings(self):
+        """Saves the configurable settings to a JSON file."""
+
+        settings_to_save = {
+            "voice": self.voice,
+            "output_format": self.output_format,
+            "model_id": self.model_id,
+            "stability": self.stability,
+            "similarity_boost": self.similarity_boost,
+            "style": self.style,
+            "use_speaker_boost": self.use_speaker_boost,
+            "speed": self.speed,
+        }
+        with open(self.output_dir / "settings.json", "w") as f:
+            dump(settings_to_save, f, indent=4)
 
     def generate_and_save_audio(self, vox_item: VoxItem) -> None:
         """Generates audio for the given text and saves it to output_path."""
 
         pr.green(vox_item.lemma_clean)
 
-        if vox_item.file_name.exists() and self.overwrite is False:
+        file_path = self.output_dir / f"{vox_item.lemma_clean}.mp3"
+
+        if file_path.exists() and self.overwrite is False:
             pr.no("skipping")
 
         else:
@@ -105,19 +142,22 @@ class ElevenLabsManager:
                     output_format=self.output_format,
                     text=f"{vox_item.devanagari}.",
                     model_id=self.model_id,
-                    voice_settings=VoiceSettings(speed=0.8, stability=0.7),
+                    voice_settings=VoiceSettings(
+                        stability=self.stability,
+                        similarity_boost=self.similarity_boost,
+                        style=self.style,
+                        use_speaker_boost=self.use_speaker_boost,
+                        speed=self.speed,
+                    ),
                 )
 
-                output_file = vox_item.file_name.with_stem(
-                    f"{vox_item.lemma_clean}_{self.voice}"
-                )
-
-                with open(output_file, "wb") as f:
+                with open(file_path, "wb") as f:
                     for chunk in response:
                         if chunk:
                             f.write(chunk)
 
                 pr.yes("saved")
+                time.sleep(self.sleep_time)  # Add 2 second pause after each iteration
 
             except Exception as e:
                 pr.no("failed")
@@ -131,6 +171,7 @@ def main():
     vox_manager = VoxManager()
     eleven_labs_manager = ElevenLabsManager()
 
+    eleven_labs_manager.save_settings()
     for vox_item in vox_manager.vox_unprocessed.values():
         eleven_labs_manager.generate_and_save_audio(vox_item)
 
