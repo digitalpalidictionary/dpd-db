092c39c78b785904fa1b786b6e859a9ce62c13aa
bdhrs <bodhirasa@gmail.com>
2025-09-25 14:41:54 +0530

    #70 deconstructor: test length of -ka suffix +2

--- go_modules/deconstructor/splitters/split_ka.go
+++ go_modules/deconstructor/splitters/split_ka.go
@@ -7,40 +7,36 @@ import (
 )
 
 func SplitKa(w data.WordData) {
+	word := w.Middle
+	w.RecurseFlag = true
+	processName := "ka"
 
-	if len(w.Middle) > 6 {
+	suffixes := []string{
+		"ko", "kā", "kāse", "kāyo", "kaṃ", "kāni", "ke",
+		"kena", "kebhi", "kehi", "kāya", "kābhi", "kāhi",
+		"kassa", "kānaṃ", "kato", "kamhā", "kasmā",
+		"kamhi", "kasmiṃ", "kesu", "kāsu"}
+	for _, suffix := range suffixes {
+		if strings.HasSuffix(string(word), suffix) && len(w.Middle) > len(suffix)+2 {
+			w.InitNewSplitter()
+			data.M.ProcessPlusOne(w)
+			suffixLen := len(suffix)
+			middle := word[:len(word)-suffixLen]
+			back := t.Str2Rune(suffix)
+			w.ToBack(middle, back)
+			processName = suffix
+			w.AddPath(processName)
 
-		w.InitNewSplitter()
-		data.M.ProcessPlusOne(w)
-		word := w.Middle
-		w.RecurseFlag = true
-		processName := "ka"
-
-		suffixes := []string{
-			"ko", "kā", "kāse", "kāyo", "kaṃ", "kāni", "ke", 
-			"kena", "kebhi", "kehi", "kāya", "kābhi", "kāhi", 
-			"kassa", "kānaṃ", "kato", "kamhā", "kasmā", 
-			"kamhi", "kasmiṃ", "kesu", "kāsu"}
-		for _, suffix := range suffixes {
-			if strings.HasSuffix(string(word), suffix) {
-				suffixLen := len(suffix)
-				middle := word[:len(word)-suffixLen]
-				back := t.Str2Rune(suffix)
-				w.ToBack(middle, back)
-				processName = suffix
-				w.AddPath(processName)
-				break
+			if data.G.IsInInflections(w.Middle) {
+				w.ToRuleBack(0)
+				w.AddWeight(2)
+				data.M.MakeMatch(processName, w)
+			} else {
+				w.ToRuleBack(0)
+				w.AddWeight(2)
+				SplitRecursive(w)
 			}
-		}
-
-		if data.G.IsInInflections(w.Middle) {
-			w.ToRuleBack(0)
-			w.AddWeight(2)
-			data.M.MakeMatch(processName, w)
-		} else {
-			w.ToRuleBack(0)
-			w.AddWeight(2)
-			SplitRecursive(w)
+			break
 		}
 	}
 }
