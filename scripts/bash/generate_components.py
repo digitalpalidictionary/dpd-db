86e0b065d96e051352119117f284394b160acc82
bdhrs <bodhirasa@gmail.com>
2025-12-20 13:17:17 +0800

    chore: update build scripts

--- scripts/bash/generate_components.py
+++ scripts/bash/generate_components.py
@@ -14,34 +14,48 @@ check_db_exists()
 COMMANDS = [
     "tools/version.py",
     "scripts/build/config_uposatha_day.py",
+
     "db/inflections/create_inflection_templates.py",
     "db/inflections/generate_inflection_tables.py",
     "scripts/build/root_has_verb_updater.py",
     "scripts/build/sanskrit_root_families_updater.py",
+    
     "db/families/family_root.py",
     "db/families/family_word.py",
     "db/families/family_compound.py",
     "db/families/family_set.py",
     "db/families/family_idiom.py",
+    
     "scripts/build/families_to_json.py",
     "scripts/build/anki_updater.py",
+    
     "db/variants/main.py",
+    
     "scripts/build/deconstructor_extract_archive.py",
     "scripts/build/deconstructor_output_add_to_db.py",
     "go run go_modules/deconstructor/main.go",
     "scripts/build/tarball_deconstructor_output.py",
+    
     "scripts/build/api_ca_eva_iti_iva_hi.py",
+    
     "db/inflections/transliterate_inflections.py",
     "db/inflections/inflections_to_headwords.py",
+    
     "db/suttas/suttas_update.py",
     "db/suttas/suttas_to_lookup.py",
+    
     "db/grammar/grammar_to_lookup.py",
+    
     "db/lookup/spelling_mistakes.py",
     "db/lookup/transliterate_lookup_table.py",
     "db/lookup/help_abbrev_add_to_lookup.py",
+    
     "scripts/build/ebt_counter.py",
+    
     "go run go_modules/frequency/main.go",
+    
     "db/epd/epd_to_lookup.py",
+    
     "scripts/build/dealbreakers.py",
 ]
 
--- scripts/bash/initial_setup_run_once.py
+++ scripts/bash/initial_setup_run_once.py
@@ -3,10 +3,19 @@
 from tools.script_runner import run_script
 
 COMMANDS = [
+    # setup config.ini file
     "tools/configger.py",
+    
+    # convert CST xml to txt
     "scripts/build/cst4_xml_to_txt.py",
+    
+    # transliterate BJT files and save to json and txt
     "scripts/build/transliterate_bjt.py",
+    
+    # make frequency maps of all the Pāḷi texts
     "go run go_modules/frequency/setup/*.go",
+    
+    # setup bold definitions database
     "db/bold_definitions/extract_bold_definitions.py",
 ]
 
