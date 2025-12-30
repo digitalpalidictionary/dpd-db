9f1cffd8e9ab6beebe030d8155ccd819ee2c4c0e
bdhrs <bodhirasa@gmail.com>
2025-12-30 08:10:22 +0800

    chore: update release notes

--- tools/docs_changelog_and_release_notes.py
+++ tools/docs_changelog_and_release_notes.py
@@ -220,7 +220,7 @@ class ChangelogGenerator:
 - {self.line_5_cell_of_pali_data}
 - {self.line_6_cells_of_root_data}
 - Pass1 complete: VIN1-4, DN1-3, MN1-3, SN1-5, AN1-11, KN1-5, KN8-9
-- Pass1 in progress: missing verbal forms
+- Pass1 in progress: VIN5
 - Pass2 complete: DN1-3, MN1-3
 - Pass2 in progress: SN1 
 - numerous additions and corrections based on user feedback
