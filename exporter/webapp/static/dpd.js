2aa54e5108e8f42e9cb970a7ae76114886a8a958
bdhrs <bodhirasa@gmail.com>
2025-12-31 20:36:54 +0800

    #98 audio: restored 'one button at a time' setting functionality, broken in bddd387

--- exporter/webapp/static/dpd.js
+++ exporter/webapp/static/dpd.js
@@ -36,8 +36,43 @@ document.addEventListener("click", function (event) {
         const target_id = otherButton.getAttribute("data-target");
         var target = document.getElementById(target_id);
         if (target) {
-            target.classList.toggle("hidden");
-            otherButton.classList.toggle("active");
+            let oneButtonToggleEnabled = false;
+            try {
+                oneButtonToggleEnabled = localStorage.getItem("one-button-toggle") === "true";
+            } catch (e) {
+                console.log("LocalStorage is not available.");
+            }
+
+            if (oneButtonToggleEnabled) {
+                var allButtons = document.querySelectorAll('.button');
+                allButtons.forEach(function (button) {
+                    if (button !== otherButton) {
+                        button.classList.remove("active");
+                    }
+                });
+
+                var allContentAreas = document.querySelectorAll('.content');
+                allContentAreas.forEach(function (contentArea) {
+                    if (contentArea !== target && !contentArea.classList.contains("summary")) {
+                        contentArea.classList.add("hidden");
+                    }
+                });
+
+                if (!target.classList.contains('summary')) {
+                    target.classList.toggle("hidden");
+                }
+            } else {
+                target.classList.toggle("hidden");
+            }
+
+            if (otherButton.classList.contains("close")) {
+                var target_control = document.querySelector('a.button[data-target="' + target_id + '"]');
+                if (target_control) {
+                    target_control.classList.toggle("active");
+                }
+            } else {
+                otherButton.classList.toggle("active");
+            }
         }
         event.preventDefault();
     }
