65a2a6490a8be9a94139efdfa29ec5243fd29ff0
bdhrs <bodhirasa@gmail.com>
2026-02-01 07:50:35 +0530

    draft release: add make_txt

--- scripts/build/config_github_release.py
+++ scripts/build/config_github_release.py
@@ -32,6 +32,7 @@ def main():
     config_update("exporter", "tarball_db", "yes")
     config_update("exporter", "make_changelog", "yes")
     config_update("exporter", "make_tpr", "yes")
+    config_update("exporter", "make_txt", "yes")
 
     config_update("anki", "update", "no")
     config_update("goldendict", "copy_unzip", "no")
