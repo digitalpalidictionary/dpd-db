5937bf5face1f2d5a16591d1845afd32ce81e89e
bdhrs <bodhirasa@gmail.com>
2026-02-12 10:51:10 +0530

    #193 mako 2 jinja: grammar update

---
+++ exporter/grammar_dict/data_classes.py
@@ -0,0 +1,45 @@
+from db.models import Lookup
+from tools.paths import ProjectPaths
+from tools.css_manager import CSSManager
+from exporter.jinja2_env import get_jinja2_env
+
+class GrammarData:
+    def __init__(self, lookup_entry: Lookup, pth: ProjectPaths, jinja_env):
+        self.headword = lookup_entry.lookup_key
+        self.grammar_data_list = lookup_entry.grammar_unpack
+        self.rows = self._process_grammar(self.grammar_data_list)
+        self.header = self._generate_header(pth, jinja_env)
+        
+    def _generate_header(self, pth: ProjectPaths, jinja_env) -> str:
+        header_templ = jinja_env.get_template("grammar_dict_header.jinja")
+        css_manager = CSSManager()
+        # The original code called render(css="", js="")
+        html_header = header_templ.render(css="", js="")
+        html_header = css_manager.update_style(html_header, "primary")
+        return html_header
+
+    def _process_grammar(self, grammar_data_list):
+        processed_rows = []
+        for headword, pos, grammar_str in grammar_data_list:
+            components = []
+            if grammar_str.startswith("reflx"):
+                parts = grammar_str.split()
+                if len(parts) >= 2:
+                    components.append(parts[0] + " " + parts[1])
+                    components += parts[2:]
+                else:
+                    components.append(grammar_str)
+            elif grammar_str.startswith("in comps"):
+                components.append(grammar_str)
+            else:
+                components = grammar_str.split()
+            
+            while len(components) < 3:
+                components.append("")
+                
+            processed_rows.append({
+                "pos": pos,
+                "components": components,
+                "headword": headword
+            })
+        return processed_rows
--- exporter/grammar_dict/grammar_dict.py
+++ exporter/grammar_dict/grammar_dict.py
@@ -2,12 +2,9 @@
 
 """Compile HTML table of all grammatical possibilities of every inflected word-form."""
 
-from mako.template import Template
-
 from db.db_helpers import get_db_session
 from db.models import Lookup
 from tools.configger import config_test
-from tools.css_manager import CSSManager
 from tools.goldendict_exporter import (
     DictEntry,
     DictInfo,
@@ -18,6 +15,8 @@ from tools.mdict_exporter import export_to_mdict
 from tools.niggahitas import add_niggahitas
 from tools.paths import ProjectPaths
 from tools.printer import printer as pr
+from exporter.jinja2_env import get_jinja2_env
+from exporter.grammar_dict.data_classes import GrammarData
 
 
 class GlobalVars:
@@ -31,7 +30,7 @@ class GlobalVars:
         self.db_session = get_db_session(self.pth.dpd_db_path)
 
         # the grammar dictionaries
-        self.html_dict: dict[str, str] = {}  # Renamed from grammar_dict_html
+        self.html_dict: dict[str, str] = {}
 
         # goldendict and mdict data_list
         self.dict_data: list[DictEntry] = []
@@ -54,9 +53,9 @@ def main():
 
     g = GlobalVars()
 
-    generate_html_from_lookup(g)  # New function replaces old ones
+    generate_html_from_lookup(g)
 
-    g.close_db()  # Close db session when done
+    g.close_db()
 
     make_data_lists(g)
     prepare_gd_mdict_and_export(g)
@@ -64,58 +63,10 @@ def main():
     pr.toc()
 
 
-def render_header_templ(
-    __pth__: ProjectPaths, css: str, js: str, header_templ: Template
-) -> str:
-    """render the html header with css and js"""
-
-    return str(header_templ.render(css=css, js=js))
-
-
-def generate_grammar_row_html(data_tuple: tuple[str, str, str]) -> str:
-    """Generate HTML for a single row in the grammar table."""
-    headword, pos, grammar_str = data_tuple
-    html_line = "<tr>"
-    html_line += f"<td><b>{pos}</b></td>"
-
-    # get grammatical_categories from grammar_str
-    grammatical_categories: list[str] = []
-    if grammar_str.startswith("reflx"):
-        parts = grammar_str.split()
-        if len(parts) >= 2:
-            grammatical_categories.append(parts[0] + " " + parts[1])
-            grammatical_categories += parts[2:]
-        else:
-            grammatical_categories.append(grammar_str)
-
-        for grammatical_category in grammatical_categories:
-            html_line += f"<td>{grammatical_category}</td>"
-    elif grammar_str.startswith("in comps"):
-        html_line += f"<td>{grammar_str}</td>"
-        html_line += "<td class='col_empty'></td>"
-        html_line += "<td class='col_empty'></td>"
-    else:
-        grammatical_categories = grammar_str.split()
-        # adding empty values if there are less than 3
-        while len(grammatical_categories) < 3:
-            grammatical_categories.append("")
-        for grammatical_category in grammatical_categories:
-            if grammatical_category == "":
-                html_line += "<td class='col_empty'></td>"
-            else:
-                html_line += f"<td>{grammatical_category}</td>"
-
-    html_line += "<td>of</td>"
-    html_line += f"<td>{headword}</td>"
-    html_line += "</tr>"
-    return html_line
-
-
 def generate_html_from_lookup(g: GlobalVars):
     """Generate HTML grammar tables from Lookup table data."""
     pr.green("querying database")
 
-    # Query the Lookup table for entries with grammar data
     lookup_results = (
         g.db_session.query(Lookup)
         .filter(Lookup.grammar.is_not(None), Lookup.grammar != "")
@@ -126,57 +77,24 @@ def generate_html_from_lookup(g: GlobalVars):
 
     pr.green("compiling html")
 
+    jinja_env = get_jinja2_env("exporter/grammar_dict")
+    template = jinja_env.get_template("grammar.jinja")
+
     html_dict = {}
-
-    # create the header from a template
-    header_templ = Template(
-        filename=str(
-            g.pth.grammar_dict_header_templ_path,
-        )
-    )
-    html_header = render_header_templ(
-        g.pth,
-        css="",
-        js="",
-        header_templ=header_templ,
-    )
-
-    # Add variables and fonts to header
-    css_manager = CSSManager()
-    html_header = css_manager.update_style(html_header, "primary")
-
-    html_table_start = "<body><div class='dpd'><table class='grammar_dict'>"
-    html_table_start += "<thead><tr><th id='col1'>pos ⇅</th><th id='col2'>⇅</th><th id='col3'>⇅</th><th id='col4'>⇅</th><th id='col5'></th><th id='col6'>word ⇅</th></tr></thead><tbody>"
-
-    # Cache for identical grammar sets
     grammar_cache: dict[str, str] = {}
 
-    # Process each lookup entry
-    for counter, lookup_entry in enumerate(lookup_results):
+    for lookup_entry in lookup_results:
         inflected_word = lookup_entry.lookup_key
         grammar_data = lookup_entry.grammar
 
         if grammar_data in grammar_cache:
-            html_body = grammar_cache[grammar_data]
+            entry_html = grammar_cache[grammar_data]
         else:
-            grammar_data_list = (
-                lookup_entry.grammar_unpack
-            )  # [(headword, pos, grammar_str)]
+            # Use ViewModel
+            data = GrammarData(lookup_entry, g.pth, jinja_env)
+            entry_html = template.render(data=data)
+            grammar_cache[grammar_data] = entry_html
 
-            html_lines = []
-            for data_tuple in grammar_data_list:
-                html_lines.append(generate_grammar_row_html(data_tuple))
-
-            html_body = "".join(html_lines)
-            grammar_cache[grammar_data] = html_body
-
-        # Assemble the full HTML for the entry
-        entry_html = (
-            html_header
-            + html_table_start
-            + html_body
-            + "</tbody></table></div></body></html>"
-        )
         html_dict[inflected_word] = entry_html
 
     g.html_dict = html_dict
@@ -188,7 +106,6 @@ def make_data_lists(g: GlobalVars):
     pr.green("making data lists")
 
     dict_data: list[DictEntry] = []
-    # Use the refactored html_dict
     for word, html in g.html_dict.items():
         synonyms = add_niggahitas([word])
 
